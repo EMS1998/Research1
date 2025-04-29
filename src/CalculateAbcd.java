d9d6d348c7742c7f4c973be883c3a2fe84a2de2f
Dooge <edwardwilliam2077@gmail.com>
2025-04-29 22:14:19 +0800

    java实验1：计算sin⁡a×cos⁡b×√(c^d )的值

---
+++ src/CalculateAbcd.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.*;//获取输入需要
+
+import com.sun.tools.javac.Main;
+
+import java.math.*; 
+public class CalculateAbcd{
+    public static void main(String[]args)throws IOException{
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("输入一个包含四个数的字符串，各数之间使用英文分号分隔，形如：a;b;c;d\n");
+        String FakeAbcd=scanner.nextLine();
+        //靓点解析：主动回收资源
+        scanner.close();
+        String[] abcd=FakeAbcd.split(";");
+
+        double a=Double.parseDouble(abcd[0]);
+        double b=Double.parseDouble(abcd[1]);
+        double c=Double.parseDouble(abcd[2]);
+        double d=Double.parseDouble(abcd[3]);
+            //sin的official document:https://docs.oracle.com/en/java/javase/23/docs/api/java.base/java/lang/Math.html#sin(double)
+            //sin⁡a×cos⁡b×√(c^d )
+            double sina=Math.sin(a);
+            double cosb=Math.cos(b);
+            //ApiOfPow:https://docs.oracle.com/en/java/javase/23/docs/api/java.base/java/lang/Math.html#pow(double,double)
+            double cd=Math.pow(c,d);
+            double sqrtCd=Math.sqrt(cd);
+                //集齐所有元素
+                double result=sina*cosb*sqrtCd;
+                System.out.println("算数结果是："+result);
+            }
+}
